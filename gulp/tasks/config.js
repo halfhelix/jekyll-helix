e687c8f279fe4e8850646547a7758cfcdaa3bdfd
peterberwind <peterberwind@gmail.com>
2016-11-18 13:31:57 -0500

    starter

---
+++ gulp/tasks/config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  sassIncludes : [
+    './node_modules/bourbon/app/assets/stylesheets',
+    './node_modules/bourbon-neat/app/assets/stylesheets'
+  ],
+  scriptFiles : [
+    './src/assets/javascript/vendor/*.js',
+    './src/assets/javascript/*.js'
+  ]
+}
